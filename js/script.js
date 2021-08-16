abb0ea5f6eec420d85fa13c9a57fa3d4eb98f7d1
Md Shakil Chowdhury <ms181161@gmail.com>
2021-08-16 16:23:23 +0600

    border radius 20px added to all card

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ const h1Tags = document.getElementsByTagName("h1");
  // backpack background color  
  const backpack = document.getElementById('backpack-section');
  backpack.style.backgroundColor='LightCyan';
+
+ //   border radius 20px of all card
+const cards = document.getElementsByClassName('card');
+for(const card of cards){
+  card.style.cssText="border-radius: 20px; border:none;";
+}
