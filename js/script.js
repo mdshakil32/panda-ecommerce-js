6393aaba416a5428b16f6a6bd0f61f06a2b516d7
Md Shakil Chowdhury <ms181161@gmail.com>
2021-08-16 16:21:29 +0600

    backpack background color added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-    // h1 tags color 
+// h1 tags color 
 const h1Tags = document.getElementsByTagName("h1");
       for(const h1 of h1Tags){
         h1.style.color='DeepPink';
       }
+
+ // backpack background color  
+ const backpack = document.getElementById('backpack-section');
+ backpack.style.backgroundColor='LightCyan';
